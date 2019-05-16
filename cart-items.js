0f1a59961e0b67a5e1d054820471f6c0d6444966
Derek <derekdenhartigh@gmail.com>
2019-05-15 22:01:28 -0400

    I think it is working, but the put keeps throwing errors

--- cart-items.js
+++ cart-items.js
@@ -3,17 +3,17 @@ const express = require("express");
 const cartItemsPage = express.Router();  // Router has to be capital
 
 let cartItems = [
-    {"id": 1, "product": "banana", "price": .15, "quantity": 99},
+    {"id": 1, "product": "banana", "price": 15, "quantity": 99},
     {"id": 2, "product": "cofee-beans", "price": 10, "quantity": 1},
     {"id": 3, "product": "dvd", "price": 5, "quantity": 4},
     {"id": 4, "product": "underpants", "price": 3, "quantity": 6},
-    {"id": 5, "product": "brocoli", "price": .32, "quantity": 2},
+    {"id": 5, "product": "brocoli", "price": 32, "quantity": 2},
     {"id": 6, "product": "shoe", "price": 45, "quantity": 1},
     {"id": 7, "product": "eye-patch", "price": 8, "quantity": 2},
 ];
 
-cartItemsPage.get("/cartItemsPage/cart-items", (req, res) => {
-    response.writeHead(200, {'content-type': "application/json"})
+cartItemsPage.get("/cartItemsPage", (req, res) => {
+    // res.writeHead(200, {'content-type': "application/json"})
     console.log("here are the items in your cart:\n")
     res.send(cartItems);
     });
@@ -26,12 +26,12 @@ cartItemsPage.post("/cartItemsPage", (req, res) => {
 // accept PUT request at URI: /cartItemsPage
 cartItemsPage.put("/cartItemsPage/:id", (req, res) => {
     res.send("altering cart item");
-    response.write(req.params.id);
+    res.write(req.params.id);
     });
 // accept DELETE request at URI: /cartItemsPage
-cartItemsPage.delete("/cartItemsPage", (req, res) => {
+cartItemsPage.delete("/cartItemsPage/:id", (req, res) => {
     res.send("Deleting a cart item");
     console.log(req.params.id);
     });
     
-module.exports = cartItems;
+module.exports = cartItemsPage;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const express = require('express');
-const cartItems = require("./cart-items.js");
+const cartItemsPage = require("./cart-items.js");
 const app = express();
 app.use(express.json());
 
-app.use("/",cartItems);
+app.use("/",cartItemsPage);
 
 const port = 3000;
 // runs the server
