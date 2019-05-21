1b1d50b62d9fec35cffc42a77c3881e222813ecc
Derek <derekdenhartigh@gmail.com>
2019-05-20 20:44:26 -0400

    going to go for the bonus features

---
+++ cart-functions.js
@@ -0,0 +1,9 @@
+// "use strict";
+// const express = require("express");
+// const cartItems = require("./cart-items");
+
+// const cartFunctions = express.Router();  // Router has to be capital
+
+
+
+// module.exports = cartFunctions;
